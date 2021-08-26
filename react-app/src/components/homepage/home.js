e5122111d7874e5200aef8570a150e718542e77e
Jonthan <cheeyongjc@gmail.com>
2021-08-25 19:00:26 -0500

    forgot to import my cabin routes to __init__, now added the routes, also added some test seed data

--- react-app/src/components/homepage/home.js
+++ react-app/src/components/homepage/home.js
@@ -1,2 +1,22 @@
 import { useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getCabinsThunk } from "../../store/cabin";
+import './home.css';
 
+function Home() {
+    const dispatch = useDispatch();
+    const cabins = useSelector(state => {
+        return Object.values(state.cabin);
+    });
+
+    useEffect(() => {
+        dispatch(getCabinsThunk());
+    }, [dispatch]);
+
+    return (
+        <>
+        
+        </>
+    )
+}
