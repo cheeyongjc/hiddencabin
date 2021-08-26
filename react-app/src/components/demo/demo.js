e714d3452b92067ad147ce1298bf2b579161294d
Jonthan <cheeyongjc@gmail.com>
2021-08-26 16:19:08 -0500

    added splash page

---
+++ react-app/src/components/demo/demo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { login } from '../../store/session';
+
+export default function Demo() {
+    const dispatch = useDispatch();
+    const demoLogin = async (e) => {
+        e.preventDefault();
+        await dispatch(login('demo@aa.io', 'password'))
+        window.location.replace('/')
+    }
+    return (
+        <button className='demoButton' onClick={(e) => demoLogin(e)}>Demo</button>
+    )
+}
--- react-app/src/components/homepage/home.js
+++ react-app/src/components/homepage/home.js
@@ -2,21 +2,40 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getCabinsThunk } from "../../store/cabin";
+import cabinReducer from "../../store/cabin";
 import './home.css';
 
 function Home() {
     const dispatch = useDispatch();
     const cabins = useSelector(state => {
-        return Object.values(state.cabin);
+        return Object.values(state.cabins);
     });
-
+    console.log(cabins);
     useEffect(() => {
-        dispatch(getCabinsThunk());
+        dispatch(getCabinsThunk())
     }, [dispatch]);
 
     return (
         <>
-        
+            <div className='cabinsContainer'>
+
+                <div className='cabinList'>
+                    {cabins.map(cabin => {
+                        return (
+                            <div className='singleCabin' key={cabin.id}>
+                                <Link to={`cabin/${cabin.id}`}>{cabin.name}</Link>
+                                {cabin.price}
+                                {cabin.guests}
+                                {cabin.beds}
+                                {cabin.description}
+                            </div>
+                        )
+                    })}
+                </div>
+
+            </div>
         </>
     )
 }
+
+export default Home;
